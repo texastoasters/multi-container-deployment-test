658a53d998674504b53d4cdd4ca4b041e4652997
Dan Horne <dan.horne@revelry.co>
2020-10-23 13:51:25 -0500

    add readme

---
+++ worker/index.js
@@ -0,0 +1,24 @@
+const keys = require('./keys')
+const redis = require('redis')
+
+const redisClient = createClient({
+    host: redisHost,
+    port: redisPort,
+    retry_strategy: () => 1000
+});
+
+const sub = redisClient.duplicate();
+
+
+// slow fib on purpose
+function fib(index) {
+    if (index < 2) return 1;
+    return fib(index -1) + fib(index - 2);
+}
+
+sub.on('message', (channel, message) => {
+    redisClient.hset('values', message, fib(parseInt(message)));
+});
+
+sub.subscribe('insert');
+
---
+++ worker/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    redisHost: process.allowedNodeEnvironmentFlags.REDIS_HOST,
+    redisPort: process.env.REDIS_PORT
+};
