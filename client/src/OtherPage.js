8410de29474627c8537c0f4554756986ca882fc8
Dan Horne <dan.horne@revelry.co>
2020-10-26 13:57:58 -0500

    fix client

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-export default function OtherPage() {
-    return (
-        <div>
-            I'm not the right page!
-            <Link to="/">Go back home!</Link>
-        </div>
-    )
-}
+export default () => {
+  return (
+    <div>
+      Im some other page!
+      <Link to="/">Go back home</Link>
+    </div>
+  );
+};
